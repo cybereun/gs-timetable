68494ba6538e6979fb7ac1814b7fa019e85a0991
cybereun <cybereunny@gmail.com>
2026-03-02 22:07:45 +0900

    Add colorful install icons and PWA manifest links

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="theme-color" content="#0284c7" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="GS Timetable" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
+    </>
+  );
+}
